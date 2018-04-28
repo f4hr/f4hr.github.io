4e7afb689b11230b0bf67e5dd08635dda29820b1
Ilya f4hr <eliasf4hr@gmail.com>
2018-04-28 18:10:30 +0500

    Update Resto
    - add content images
    - add featured block slider
    - fix index.html

--- resto/js/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var navList = document.querySelector('.main-nav__list');
-var navToggle = document.querySelector('.main-nav__toggle');
-
-navToggle.addEventListener('click', function () {
-  navList.classList.toggle('main-nav__list--opened');
-  navToggle.classList.toggle('main-nav__toggle--opened');
-});
